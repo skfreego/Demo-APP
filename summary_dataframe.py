578a284ec2113a3560f8f65d22a0bf485206b6c5
Suvin Koshy George <70506817+skfreego@users.noreply.github.com>
2023-04-18 12:06:55 +0530

    Updated

--- summary_dataframe.py
+++ summary_dataframe.py
@@ -1,147 +1,134 @@
-import os
-import csv
-import time
-import statistics
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import urllib.request
+import urllib.parse
+import json
+import time
+from datetime import datetime
 
 
-def process_csv_files(input_folder):
-    # Initialize the total row count, sensor count, and dictionaries to store sensor time intervals
-    total_rows = 0
-    sensor_counts = {}
-    sensor_avg_times = {}
-    sensor_max_times = {}
-    sensor_min_times = {}
-    unique_sensor_ids = set()
-    # Initialize a list to store the file names that have already been processed
-    processed_files = []
+def convert_to_unix_timestamp(timestamp_str):
+    try:
+        # Try to parse the input as a Unix timestamp (integer or float)
+        timestamp = float(timestamp_str)
+        return int(timestamp)
+    except ValueError:
+        # Try to parse the input as a datetime string
+        for fmt in ['%m/%d/%Y %H:%M:%S', '%Y/%m/%d %H:%M:%S', '%Y-%m-%dT%H:%M:%S.%fZ', '%d/%m/%Y %H:%M:%S',
+                    '%Y-%m-%d %H:%M:%S', '%Y-%m-%d %H:%M:%S.%f']:
+            try:
+                dt = datetime.strptime(timestamp_str, fmt)
+                return int(dt.timestamp())
+            except ValueError:
+                pass
+        raise ValueError(f"No matching format found for timestamp {timestamp_str}")
+
+
+def run_app():
+    st.header("Sensor Data Analysis Report Dashboard")
+
+    # Define API parameters
+    url = "http://gapp.agverse.in/api/v1/get-telemetry-data"
+    start_id = 1
+    id_interval = 100000
+    # Define time interval parameters
+    interval_seconds = 3
+    previous_id = None
+
+    # Create empty containers to display results
+    containers = {
+        'Total Number of Records': [st.subheader("1. Total Number of Records"), st.empty()],
+        'Unique Mac IDS': [st.subheader("2. Unique Mac IDS"), st.empty()],
+        'Sensor ID and Its Count': [st.subheader("3. Sensor ID and Its Count"), st.empty()],
+        'Summary Statistics of Sensor ID': [st.subheader("4. Summary Statistics of Sensor ID"), st.empty()]
+    }
+
+    # Create empty DataFrame to store data
+    all_data = pd.DataFrame()
 
     while True:
-        # Get a list of CSV files in the input folder that have not been processed before
-        csv_files = [filename for filename in os.listdir(input_folder) if filename.endswith('.csv')
-                     and filename not in processed_files]
+        try:
+            # Update start and end IDs based on time interval
+            if previous_id is None:
+                previous_id = start_id
+                end_id = previous_id + id_interval
+            else:
+                start_id = previous_id
+                previous_id = end_id
+                end_id = previous_id + id_interval
 
-        if not csv_files:
-            print('There are no CSV files in the input folder.')
-            time.sleep(15)
-            continue
+            # Fetch data from API
+            params = {"id": f"{start_id}/{start_id + id_interval - 1}"}
+            url_encoded_params = urllib.parse.urlencode(params)
+            url_with_params = f"{url}?{url_encoded_params}"
+            url_auth = urllib.request.Request(url_with_params,
+                                              headers={"username": "gapp-apis", "password": "4Score&7yrsAgo"})
+            response = urllib.request.urlopen(url_auth)
+            data = response.read()
+            values = json.loads(data)
+            telemetry_data = values.get("result", [])
+            df = pd.DataFrame(telemetry_data)
 
-        # Process each CSV file
-        for csv_file in csv_files:
-            # Open the CSV file for reading
-            with open(os.path.join(input_folder, csv_file), 'r') as csvfile:
-                reader = csv.reader(csvfile)
-                # Skip the header row
-                header_row = next(reader, None)
-                if header_row is None:
-                    print(f'The file {csv_file} is empty or contains only a header row.')
-                    continue
+            if not df.empty:
+                # Convert timestamps to Unix timestamps
+                df['timestamp_unix'] = df['timestamp'].apply(convert_to_unix_timestamp)
 
-                # Count the number of rows and sensor IDs in the file
-                for row in reader:
-                    sensor_id = row[1]
-                    total_rows += 1
-                    if sensor_id == '' or sensor_id == '00':
-                        continue  # Skip rows with empty or "00" sensor ID
-                    if sensor_id in sensor_counts:
-                        sensor_counts[sensor_id] += 1
-                    else:
-                        sensor_counts[sensor_id] = 1
-                        unique_sensor_ids.add(sensor_id)
+                # Calculate time difference
+                df['time_difference (s)'] = df['timestamp_unix'].diff()
 
-                    if row[3] == '':
-                        print(f'Time interval value is empty in file {csv_file}, row {total_rows}.')
-                        continue
+                all_data = all_data.append(df)
+                all_data = all_data.drop_duplicates()
 
-                    try:
-                        time_interval = float(row[3])
-                    except ValueError:
-                        print(f'Invalid time interval value in file {csv_file}, row {total_rows}: {row[3]}')
-                        continue
+                # Display total number of records
+                num_records = all_data.shape[0]
+                containers['Total Number of Records'][1].write(f"Total number of records: **{num_records}**")
 
-                    if sensor_id in sensor_avg_times:
-                        sensor_avg_times[sensor_id].append(time_interval)
-                        sensor_max_times[sensor_id] = max(sensor_max_times[sensor_id], time_interval)
-                        sensor_min_times[sensor_id] = min(sensor_min_times[sensor_id], time_interval)
-                    else:
-                        sensor_avg_times[sensor_id] = [time_interval]
-                        sensor_max_times[sensor_id] = time_interval
-                        sensor_min_times[sensor_id] = time_interval
+                # Display unique mac ids
+                unique_sensor_ids = all_data['mac_id'].str.upper()
+                unique_sensor_ids = unique_sensor_ids.str.replace('[^A-F0-9:]+', '', regex=True)
+                unique_sensor_ids = unique_sensor_ids[unique_sensor_ids.str.contains(':')].value_counts()
+                unique_sensor_ids = unique_sensor_ids[(unique_sensor_ids != 1)]
+                containers['Unique Mac IDS'][1].write(
+                    f"Total number of unique Mac IDs (excluding those with count of 1 and 2): {len(unique_sensor_ids)}")
 
-        # Add the processed file name to the list
-            processed_files.append(csv_file)
+                # Display unique Sensor IDs and their counts
+                sensor_counts = all_data['mac_id'].str.upper().value_counts()
+                sensor_counts_df = pd.DataFrame({'mac_id': sensor_counts.index, 'Count': sensor_counts.values})
+                sensor_counts_df = sensor_counts_df[sensor_counts_df['Count'] > 1] # Exclude mac_id with count of 1
+                sensor_counts_df['mac_id'] = sensor_counts_df['mac_id'].str.upper().str.replace('[^A-F0-9:]+', '',
+                                                                                                regex=True)
+                sensor_counts_df = sensor_counts_df[sensor_counts_df['mac_id'].str.contains(':')]
+                sensor_counts_df = sensor_counts_df.drop_duplicates()
+                if not sensor_counts_df.empty:
+                    containers['Sensor ID and Its Count'][1].write(sensor_counts_df)
+                else:
+                    containers['Sensor ID and Its Count'][1].write("No Sensor ID with count > 2 found.")
 
-        # Construct a pandas dataframe from the collected data
-        data = {
-            'Sensor ID': [':'.join([s_id.upper().zfill(2) for s_id in sensor_id.split(':')])
-                          for sensor_id in sensor_avg_times.keys() if sensor_id != '' and sensor_id != '00'],
-            'Count': [sensor_counts[sensor_id] for sensor_id in sensor_counts.keys()
-                      if sensor_id != '' and sensor_id != '00'],
-            'Average Interval': [round(statistics.mean(sensor_avg_times[sensor_id]), 2)
-                                 for sensor_id in sensor_avg_times.keys()
-                                 if sensor_id != '' and sensor_id != '00'],
-            'Maximum Interval': [int(round(sensor_max_times[sensor_id]))
-                                 for sensor_id in sensor_max_times.keys() if sensor_id != '' and sensor_id != '00'],
-            'Minimum Interval': [int(round(sensor_min_times[sensor_id]))
-                                 for sensor_id in sensor_min_times.keys()
-                                 if sensor_id != '' and sensor_id != '00']
-        }
+                # Calculate summary statistics of sensor IDs
+                # sensor_stats = df.groupby('mac_id')['time_difference (s)'].agg(['mean', 'max', 'min']).reset_index()
+                sensor_stats = all_data.groupby(all_data['mac_id'].str.upper())['time_difference (s)']\
+                    .agg(['mean', 'max', 'min']).reset_index()
+                sensor_stats.rename(
+                    columns={'mean': 'Mean Interval', 'max': 'Maximum Interval', 'min': 'Minimum Interval'},
+                    inplace=True)
+                sensor_stats['Maximum Interval'] = sensor_stats['Maximum Interval'].fillna(0).astype(int)
+                sensor_stats['Minimum Interval'] = sensor_stats['Minimum Interval'].fillna(0).astype(int)
+                sensor_stats['Mean Interval'] = sensor_stats['Mean Interval'].apply(lambda x: '{:.2f}'.format(x))
+                sensor_stats = sensor_stats[
+                    sensor_stats['mac_id'].isin(sensor_counts_df['mac_id'])]  # Exclude mac_id with count of 2
+                sensor_stats['mac_id'] = sensor_stats['mac_id'].str.upper().str.replace('[^A-F0-9:]+', '', regex=True)
+                sensor_stats = sensor_stats[sensor_stats['mac_id'].str.contains(':')]
+                if not sensor_stats.empty:
+                    containers['Summary Statistics of Sensor ID'][1].write(sensor_stats)
+                else:
+                    containers['Summary Statistics of Sensor ID'][1].write("No Sensor ID with count > 2 found.")
+            # Sleep for 1 second before checking for new CSV files again
+            time.sleep(interval_seconds)
 
-        df = pd.DataFrame(data)
-
-
-
-        st.write('Total records:', total_rows, '\n')
-        st.write('Unique Sensor IDs:', len(unique_sensor_ids), '\n')
-
-
-
-        # Print the sensor ID and count
-        st.write('Sensor ID  and Count')
-        count_sensors_df = pd.DataFrame({
-            'Sensor ID': [':'.join([s_id.upper().zfill(2) for s_id in sensor_id.split(':')]) for sensor_id in
-                          sorted(sensor_avg_times.keys()) if sensor_id not in ['', '00']],
-            'Count': [sensor_counts[sensor_id] for sensor_id in sorted(sensor_counts.keys())
-                      if sensor_id != '' and sensor_id != '00']
-            })
-        # Display as table
-        st.write(count_sensors_df)
-
-        # Print the Summary statistics
-        st.write('Summary Statistics')
-        summary_stats_df = pd.DataFrame({
-            'Sensor ID': [':'.join([s_id.upper().zfill(2) for s_id in sensor_id.split(':')]) for sensor_id in
-                          sorted(sensor_avg_times.keys()) if sensor_id not in ['', '00']],
-            'Average Interval': [statistics.mean(sensor_avg_times[sensor_id]) for sensor_id in
-                                 sorted(sensor_avg_times.keys()) if sensor_id not in ['', '00']],
-            'Maximum Interval': [int(round(sensor_max_times[sensor_id]))
-                                 for sensor_id in sorted(sensor_max_times.keys())
-                                 if sensor_id not in ['', '00']],
-            'Minimum Interval': [int(round(sensor_min_times[sensor_id]))
-                                 for sensor_id in sorted(sensor_min_times.keys())
-                                 if sensor_id not in ['', '00']]
-            })
-
-        # Format the average interval values as strings with 2 decimal places
-        summary_stats_df['Average Interval'] = summary_stats_df['Average Interval'].apply(lambda x: "{:.2f}".format(x))
+        except Exception as e:
+            print(f"Error occurred: {str(e)}")
+            break
 
 
-        st.write(summary_stats_df)
-
-
-# Wait for 5 seconds before checking for new files again
-time.sleep(5)
-
-
-def streamlit_app():
-    st.title('Sensor Data Dashboard')
-    st.write('')
-    output_text = st.empty()
-    while True:
-        output_text.text(process_csv_files('.'))
-        time.sleep(5)
-
-
-# Run the Streamlit app
-streamlit_app()
+if __name__ == '__main__':
+    run_app()
